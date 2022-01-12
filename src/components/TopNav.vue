35f8662bd2ef7e2e5318fc5d2a5f6392ac32d893
Aaron Alley <aaronalleydev@gmail.com>
2022-01-12 12:51:04 +0000

    still adding to the nav - fixed alignment of links

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,13 +1,12 @@
 <template>
 <nav class="flex items center bg-red-300 p-3 flex-wrap">
   <router-link to="/" class="p-2 mr-4 inline-flex items-center">
-  <img src="../assets/logo.png" alt="" class="w-12 h-12">
-  <span class="md:text-xl text-red-900 font-bold uppercase tracing-wide">Alley S + N</span>
+  <img src="../assets/img/AlleyLogoClear.png" alt="" class="w-36 h-24">
   </router-link>
   <button @click="menuOpen" class="text-red-900 inline-flex items-center p-3 roundeed lg:hidden ml-auto">
     <span class="material-icons">menu</span>
   </button>
-  <div :class="{toggleNav: open}" class="top-nav w-full lg:inline-flex lg:flex-grow lg:w-auto" id="navigation">
+  <div :class="{toggleNav: open}" class="text-center text-xl w-full lg:inline-flex lg:flex-grow lg:w-auto lg:pt-5" id="navigation">
     <div class="lg:inline-flex lg:flex-row lg:ml-auto">
       <ul class="flex flex-col lg:flex-row">
         <li v-for="link in links" :key="link">
@@ -39,9 +38,11 @@ export default {
 </script>
 
 <style>
-/* li .router-link-active {
-  border-bottom: 1px solid black;
-} */
+li .router-link-active {
+  padding-bottom: 0;
+  border-bottom: 2px solid red;
+  border-bottom-width: 90;
+}
 
 .toggleNav {
   display: none;
