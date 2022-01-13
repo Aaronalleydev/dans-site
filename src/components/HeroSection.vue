10c00fe9d5dd6554d77b78e16415cd9fa4585d00
Aaron Alley <aaronalleydev@gmail.com>
2022-01-13 22:29:52 +0000

    messing around with the hero section

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,6 +1,15 @@
 <template>
-<div>
-  <img class="w-44 h" src="../assets/img/fbHeroNoBg.png" alt="A picture of dan">
+<div class="grid grid-cols-2 grid-rows-2 max-w-screen-xl m-auto bg-red-100">
+  <div class="row-span-2">
+    <img class="" src="../assets/img/AlleyLogoClear.png" alt="A picture of dan">
+  </div>
+  <div class="bgImg flex flex-col justify-center items-center">
+    <h1 class="text-4xl">Hero section text</h1>
+    <p>Super catchy tag line</p>
+  </div>
+  <div class="flex justify-center items-center">
+    <button class="bg-indigo-700 text-4xl p-3 rounded-full">Call to action</button>
+  </div>
 </div>
 </template>
 
@@ -11,5 +20,8 @@ export default {
 </script>
 
 <style>
-
+.bgImg {
+  background-image: url('../assets/img/hero3.jpeg');
+  opacity: 0.8;
+}
 </style>
