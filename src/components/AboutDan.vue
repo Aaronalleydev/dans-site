c5cfc0efd535a6599f1d61b5f02a7a564afeeb13
Aaron Alley <aaronalleydev@gmail.com>
2022-01-14 22:39:47 +0000

    messing about with the about section

---
+++ src/components/AboutDan.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="bg-red-200">
+<h1 class="text-4xl text-red-800 font-bold text-center pt-8 pb-6">About Dan</h1>
+  <p>A qualified personal trainer, registered nutritionist and boxing coach. I come from an endurance & combat sport background. Now working at one of the leading health clubs in Europe and with clients in the UK, Europe & North America. My goal is to help you achieve your goals whatever they may be. I believe in a flexible approach to training and nutrition and want to help show you how you can enjoy your training and life at the same time.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="home">
     <HeroSection />
+    <AboutDan />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HeroSection from '../components/HeroSection.vue'
+import AboutDan from '../components/AboutDan.vue'
 
 
 export default {
   name: "Home",
-  components: { HeroSection },
+  components: { HeroSection, AboutDan },
 };
 </script>
